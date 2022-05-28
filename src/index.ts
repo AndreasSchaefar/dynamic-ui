37879070c579d5737824864ecfb6f7b1c1618f36
AndreasSchaefar <skachko.andrii@gmail.com>
2022-05-28 18:03:30 +0300

    initialized a dropdown with arbitrary options

---
+++ src/index.ts
@@ -0,0 +1,6 @@
+import { DropDown, Carousel, MobileMenu } from './dynamicComponents';
+import './sass/main.scss';
+
+const items = ['Item 1', 'Item 2', 'Item 3', 'Item 4', 'Item 5'];
+
+const dropDown = new DropDown(items);
